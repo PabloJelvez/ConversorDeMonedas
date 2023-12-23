4ffd1721787034617e1b6a4955bf7989431eb27c
Pablo Jelvez <pjelvezj@gmail.com>
2023-12-23 07:37:41 -0300

    Primera implementacion del grafico

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -3,15 +3,17 @@ const optionExchange = document.getElementById("selectExchange")
 const inputExchange = document.getElementById("clpValue")
 const buttonExchange = document.getElementById("btnExchange")
 const resultExchange = document.getElementById("exchangeValue")
+const chartCanvas = document.getElementById("chart") 
+
 
 let exchanges
 
 async function getExchange(){
     try{
     const response = await fetch(urlApi)
-    const exchanges = await response.json()
+    exchanges = await response.json()
     return exchanges
-    }catch{
+    }catch(error){
     const elementError = document.getElementById("error")
     elementError.innerHTML = `!Algo salio mal¡ Error:  ${error.message}`
     }
@@ -19,16 +21,16 @@ async function getExchange(){
 } 
 
 async function renderExchange(){
- exchanges = await getExchange()
+ await getExchange()
  let template = ""
  const optionVisible = ["dolar", "euro"]
 const filteredExchange = Object.values(exchanges).filter((exchange) =>{
     return optionVisible.includes(exchange.codigo)
 })
-
+optionExchange.innerHTML = ""
 filteredExchange.forEach((exchange) => {
         template += `
-        <option value="${exchange.codigo}" id="${exchange.codigo}">${exchange.nombre}</option>
+        <option value="${exchange.valor}" id="${exchange.codigo}">${exchange.nombre}</option>
         `
     });
     optionExchange.insertAdjacentHTML("beforeend", template)
@@ -36,17 +38,158 @@ filteredExchange.forEach((exchange) => {
 
 renderExchange()
 
-function convertClp() {
+async function convertClp() {
     const clp = Number(inputExchange.value)
-    const exchangeType = optionExchange.value
-    const exchange = exchanges[exchangeType].valor
+    const exchange = Number(optionExchange.value)
     const result = (clp * (1 / exchange))
     resultExchange.textContent = `Resultado: $${result.toFixed(2)}`
     inputExchange.value = ""
   }
+  let chart
+  async function createChart() {
+   
+    const exchangeCode = optionExchange.options[optionExchange.selectedIndex].id
+    
+    const exchangeData = await fetch(`https://mindicador.cl/api/${exchangeCode}`)
+    const exchangeJson = await exchangeData.json()
+   
+    const labels = []
+    const values = []
+   
+    for (let i = exchangeJson.serie.length - 10; i < exchangeJson.serie.length; i++) {
+       
+        const date = new Date(exchangeJson.serie[i].fecha)
+        const value = exchangeJson.serie[i].valor
+        
+        const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
+       
+        labels.push(formattedDate)
+        values.push(value)
+    }
   
-  buttonExchange.addEventListener("click", convertClp)
+    const ctx = chartCanvas.getContext("2d")
+    if (chart) {
+        chart.destroy()
+    }
+   
+     chart = new Chart(ctx, {
+       
+        type: "line",
+        
+        data: {
+            labels: labels,
+            datasets: [{
+                label: exchangeJson.nombre, 
+                data: values, 
+                borderColor: "blue",
+                fill: false 
+            }]
+        },
+        
+        options: {
+            
+            maintainAspectRatio: false,
+            
+            legend: {
+                display: true
+            },
+            
+            title: {
+                display: true,
+                text: "Valores de los últimos 10 días"
+            },
+            
+            scales: {
+                
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        unit: "day",
+                        displayFormats: {
+                            day: "DD/MM/YYYY"
+                        }
+                    }
+                }],
+                
+                yAxes: [{
+                    ticks: {
+                        
+                        callback: function(value, index, values) {
+                            return value.toFixed(2)
+                        }
+                    }
+                }]
+            }
+        }
+    })
+}
 
+renderExchange()
+
+buttonExchange.addEventListener("click", function(){
+     convertClp()
+     createChart()
+})
+
+//optionExchange.addEventListener("change", createChart)
+
+
+
+
+  
+  //async function getData(exchanges){
+    
+    //await getExchange()
+    //const exchangeType = optionExchange.value
+    //const exchangeData = await exchanges[exchangeType].serie
+    
+    //const labels = exchangeData.map((exchange) => {
+      //  return  new Date(exchange.fecha).toISOString().slice(0, 10)
+    //})
+    //const data = exchangeData.map((exchange) =>{
+      //  const value = exchange.valor
+        //return Number(value)
+    //})
+    //const datasets = [
+      //  {
+        //label: "Moneda",
+        //borderColor: "rgb(255, 99, 132)",
+        //data
+    //    }
+    //]
+    //return {labels, datasets}
+  //}
+
+  //async function renderGrafica(){
+  //  const data = await getData(exchanges)
+  //  const config = {
+  //      type: "line",
+  //      data: {
+   //         labels: data.labels,
+   //         datasets: [{
+   //             label: data.datasets[0].label,
+     //           borderColor: data.datasets[0].borderColor,
+       //         data: data.datasets[0].data
+   //         }]
+   //     }
+   // }
+   // const chartElement = document.getElementById("myChart")
+   // chartElement.style.backgroundColor = "white"
+   // if (myChart) {
+     //   myChart.destroy()
+    //}
+    //myChart = new Chart(chartElement, config)
+//}
+
+
+  
+
+
+ // buttonExchange.addEventListener("click", async function() {
+   // convertClp()
+   // await renderGrafica()
+  //})
+  
 
 
 
